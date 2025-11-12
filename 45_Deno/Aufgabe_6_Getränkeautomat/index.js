a438d3482e5a4bd58382892020a021ed1449d946
danilo <danilo@example.com>
2025-11-12 16:01:46 +0100

    Aufgabe_6_Getränkeautomat finished

---
+++ 45_Deno/Aufgabe_6_Getränkeautomat/index.js
@@ -0,0 +1,17 @@
+let a = prompt("Welches Getränk möchten Sie? (Cola, Wasser, Bier, Tee):");
+
+if (a == "Cola") {
+    console.log("Danke für deine Bestellung von " + a);
+}
+
+else if (a == "Bier") {
+    console.log("Danke für deine Bestellung von " + a);
+}
+
+else if (a == "Wasser") {
+    console.log("Danke für deine Bestellung von " + a);
+}
+
+else if (a == "Tee") {
+    console.log("Danke für deine Bestellung von " + a);
+}

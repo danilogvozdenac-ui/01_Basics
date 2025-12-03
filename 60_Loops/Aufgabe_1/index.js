9484ae21e7464c46cee1596487d2ca82e17314f5
danilo <danilo@example.com>
2025-12-03 16:31:21 +0100

    finished Aufgabe_2 (60_Loops)

--- 60_Loops/Aufgabe_1/index.js
+++ 60_Loops/Aufgabe_1/index.js
@@ -1,6 +1,6 @@
 let b = 0;
 
-for(let a = 1; a <= 100; a++) {
+for(let a = 1; a <= 10; a++) {
     b = b + a;
 }       
 console.log(b);
---
+++ 60_Loops/Aufgabe_2/index.js
@@ -0,0 +1,8 @@
+let b = 0;
+
+for (let a = 0; a <= 50; a+= 2) {
+    b = a + b;
+}  
+
+console.log(b);
+
